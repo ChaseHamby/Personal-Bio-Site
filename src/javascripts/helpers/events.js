d26bfb514f8565df33531be23bcf2ad95933924d
Chase Hamby <c.chase.hamby@gmail.com>
2018-11-02 09:43:18 -0500

    working on buttons and astyling

---
+++ src/javascripts/helpers/events.js
@@ -0,0 +1,15 @@
+import $ from 'jquery';
+
+// const bioButton = $('#bioButton');
+
+// const projectButton = () => {
+//   $('#mainDiv').on('click', '#project-link', () => {
+//     $('#project-container').show();
+//   });
+// };
+
+// const setEvents = () => {
+//   projectButton();
+// };
+
+// export default { setEvents };
