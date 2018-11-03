a52f571fa623795315e9b36347aec4fdf80225e3
Chase Hamby <c.chase.hamby@gmail.com>
2018-11-03 14:01:05 -0500

    styling

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-import 'jquery';
+import $ from 'jquery';
 import 'bootstrap';
 import projectView from './javascripts/components/projects';
 import './index.scss';
+import familyPhoto from './images/family.jpg';
+
+$('#familyPhoto').attr('src', familyPhoto);
 
 const initializeMainView = () => {
   projectView.initializeProjectView();
-  // events.setEvents();
 };
 
 initializeMainView();
